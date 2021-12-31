3fe847d1516a849edeabdbae56087402df5398ab
Kenneth Wang <yunfeiw@me.com>
2021-12-31 09:23:00 +0000

    digit option function made

--- Lambda-function/convertVanity.js
+++ Lambda-function/convertVanity.js
@@ -29,3 +29,31 @@ const covertVanity=string =>{
   const vanityNumber = `${string.slice(0, 1)}-${string.slice(1, 4)}-${lastSevenVanity}`
   return vanityNumber;
 }
+
+
+const digitOption= string => {
+  let option;
+  if(string ==='0'){
+    option=['0']
+  } else if (string === '1') {
+    vanityOptions.push('1')
+  } else if (string === '2') {
+    option =['A', 'B', 'C']
+  } else if (string === '3') {
+    option =['D', 'E', 'F']
+  } else if (string === '4') {
+    option =['G', 'H', 'I']
+  } else if (string === '5') {
+    option =['J', 'K', 'L']
+  } else if (string === '6') {
+    option =['M', 'N', 'O']
+  } else if (string === '7') {
+    option =['P', 'Q', 'R', 'S']
+  } else if (string === '8') {
+    option =['T', 'U', 'V']
+  } else if (string === '9') {
+    option =['W', 'X', 'Y', 'Z']
+  }
+
+  return option
+}
